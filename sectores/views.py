d9171ae950c0e68ffdc6fa07c27258d429561999
Matías Ingrassia <matiasingrassia@gmail.com>
2026-04-17 10:56:05 -0300

    Add sectores

--- sectores/views.py
+++ sectores/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    context = {"mensaje": "Bienvenidos a mi applicacion"}
+    context = {"mensaje": "Bienvenidos a mi aplicacion"}
     return render(request, "sectores/index.html", context)
