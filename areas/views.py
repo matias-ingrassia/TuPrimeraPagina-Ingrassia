e61f04a6e47f4e814f0821a990997dc853457b45
Matías Ingrassia <matiasingrassia@gmail.com>
2026-04-17 11:05:49 -0300

    rename sectores to areas

--- areas/views.py
+++ areas/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     context = {"mensaje": "Bienvenidos a mi aplicacion"}
-    return render(request, "sectores/index.html", context)
+    return render(request, "areas/index.html", context)
--- sectores/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SectoresConfig(AppConfig):
-    name = 'sectores'
